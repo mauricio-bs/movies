70d3bcc3ce4f388b073f5d999bd1233db3bc9bbd
mauricio-bs <mauricio.3g2014@gmail.com>
2021-07-27 11:34:29 -0300

    Set database configurations

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,12 @@
 module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'development', // Username
-    password: 'Noukchott@1679', // Password
-    database: 'movies',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'development', // Username
+  password: 'Noukchott@1679', // Password
+  database: 'movies',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
   }
-  
+}
