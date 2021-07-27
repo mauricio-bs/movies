4d572f3ce1329de5ac2548f625eae9b6edc15889
mauricio-bs <mauricio.3g2014@gmail.com>
2021-07-27 12:56:12 -0300

    Set controllers and methods for each route

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,31 @@
 import { Router } from 'express'
 
 // Controllers
+import ViewerController from './app/controller/ViewerController'
+import MovieController from './app/controller/MovieController'
 
 const routes = new Router()
 
 /* VIEWERS */
 
 // Show all spectators
-routes.get('/viewers')
+routes.get('/viewers', ViewerController.index)
 // Show 1 viewer
-routes.get('/viewers/:id')
+routes.get('/viewers/:id', ViewerController.show)
 // Create a viewer
-routes.post('/viewers')
+routes.post('/viewers', ViewerController.store)
 // Set a movie as seen (Viewer id as URL param, and movie ID on body)
-routes.patch('/viewers/:id')
+routes.patch('/viewers/:id', ViewerController.update)
+// Delete a Viewer
+routes.delete('/viewers/:id', ViewerController.delete)
 
 /* MOVIES */
 
 // Show all movies
-routes.get('/movies')
+routes.get('/movies', MovieController.index)
 // Show 1 movie
-routes.get('/movies/:id')
+routes.get('/movies/:id', MovieController.show)
 // Creat a movie
-routes.post('/movies')
+routes.post('/movies', MovieController.store)
+// Delete a movie
+routes.delete('/movies/:id', MovieController.delete)
