f2506b35679e26375faff2916a874a5722b3bf18
mauricio-bs <mauricio.3g2014@gmail.com>
2021-08-14 13:50:11 -0300

    Configure heroku port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import app from './app'
+require('dotenv/config')
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server running on http://localhost:3000')
 })
