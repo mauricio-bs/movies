d89192094c0bd8c51d8bd116bf3c3c3e742f1206
mauricio-bs <mauricio.3g2014@gmail.com>
2021-08-14 13:35:10 -0300

    Added CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 import './database'
 
 class App {
@@ -12,6 +13,8 @@ class App {
 
   middlewares() {
     this.app.use(express.json())
+
+    this.app.use(cors())
   }
 
   routes() {
