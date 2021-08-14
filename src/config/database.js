4c8f9bec640474f4fcbdeae45563ab79ce11bc9a
mauricio-bs <mauricio.3g2014@gmail.com>
2021-08-14 15:30:44 -0300

    Created uri database connection

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,11 @@ require('dotenv/config')
 // Database configurations
 module.exports = {
   dialect: 'mysql',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  uri: process.env.CLEARDB_DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
